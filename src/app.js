e2e1f3f757c932dda710704eada3cbdd8d07a752
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2026-03-23 23:54:54 +0530

    feat(app): add habit route in app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import habitRoutes from "./routes/habit.route.js";
 
 const app = express();
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+app.use('/api/habits', habitRoutes);
 app.get('/health', (req, res)=>{
     res.status(200).json({status: "ok"})
 })
