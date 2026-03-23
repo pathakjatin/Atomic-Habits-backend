531dc5e3ee61facdb8e3d01d19bb1aef504945f9
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2026-03-23 21:59:28 +0530

    feat(auth): implement register and login service logic

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,58 +1,41 @@
 import User from "../models/user.model.js";
-import  generateToken  from "../utils/generateToken.utils.js";
+import AppError from "../utils/AppError.js";
 
-export async function registerUser(payload){
-    const{
-        name, 
-        email,
-        picture,
-        phone,
-        username,
-        password,
-        dob, 
-        gender
-    } = payload;
-    const existingUser = await User.findOne({
-        $or : [{email}, {username}]
-    });
-    if(existingUser){
-        throw new Error("USER_ALREADY_EXISTS");
+export async function registerUser(payload) {
+    const { name, email, picture, phone, username, password, dob, gender } = payload;
+
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+    if (existingUser) {
+        throw new AppError("USER_ALREADY_EXISTS", "Email or username is already taken", 409);
     }
 
-    const user = await User.create({
-        name, 
-        email,
-        picture,
-        phone,
-        username,
-        password,
-        dob, 
-        gender
-    });
-    return{
-        id:user._id,
-        name:user.name,
-        email:user.email,
-        username:user.username
-    }
+    const user = await User.create({ name, email, picture, phone, username, password, dob, gender });
+
+    return {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        username: user.username,
+    };
 }
 
-export async function loginUser (payload){
-    const{username, password} = payload;
-    // console.log(username)
-    const existingUser = await User.findOne({username}).select("+password");
-    if(!existingUser){
-        throw new Error("INVALID_CREDENTIALS");
+export async function loginUser(payload) {
+    const { username, password } = payload;
+
+    const existingUser = await User.findOne({ username }).select("+password");
+    if (!existingUser) {
+        throw new AppError("INVALID_CREDENTIALS", "Invalid username or password", 401);
     }
+
     const isMatch = await existingUser.matchPassword(password);
-    if(!isMatch){
-        throw new Error("INVALID_CREDENTIALS");
+    if (!isMatch) {
+        throw new AppError("INVALID_CREDENTIALS", "Invalid username or password", 401);
     }
-    return{
+
+    return {
         _id: existingUser._id,
         name: existingUser.name,
         email: existingUser.email,
-        username : existingUser.username,
-        token : generateToken(existingUser._id)
-    }
+        username: existingUser.username,
+    };
 }
